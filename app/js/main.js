96350b05e700ef7b2b1e8c0a8a4714ced3588e6d
Ruslan <molodoybaja@gmail.com>
2016-05-26 12:45:30 +0300

    sixth

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,34 @@
-var module = (function(){
+var module = (function () {
     var burgerMenu = document.querySelector('.burger-wrap__menu-burger-wrap');
-    var crossMenu = document.querySelector('.burger-wrap__menu-cross-wrap')
+    var crossMenu = document.querySelector('.burger-wrap__menu-cross-wrap');
     var nav = document.querySelector('.navigation');
-    var navList = document.querySelector('.navigation__list');
+    var navList =    document.querySelector('.navigation__list');
 
-    burgerMenu.addEventListener('click', function(e){
+    burgerMenu.addEventListener('click', function (e) {
         burgerMenu.classList.add('menu-burger-wrap--mod-js');
-        crossMenu.classList.add('menu-cross-wrap--mod-js')
+        crossMenu.classList.add('menu-cross-wrap--mod-js');
         nav.classList.add('navigation--mod-js');
-        navList.classList.add('navigation__list--mod-js');
-    })
+        navList.classList.add( 'navigation__list--mod-js');
+    });
 
-    crossMenu.addEventListener('click', function(e){
+    crossMenu.addEventListener('click', function (e) {
         burgerMenu.classList.remove('menu-burger-wrap--mod-js');
-        crossMenu.classList.remove('menu-cross-wrap--mod-js')
+        crossMenu.classList.remove('menu-cross-wrap--mod-js');
         nav.classList.remove('navigation--mod-js');
         navList.classList.remove('navigation__list--mod-js');
-    })
+    });
 
-    burgerMenu.addEventListener('touchstart', function(e){
+    burgerMenu.addEventListener('touchstart', function (e) {
         burgerMenu.classList.add('menu-burger-wrap--mod-js');
-        crossMenu.classList.add('menu-cross-wrap--mod-js')
+        crossMenu.classList.add('menu-cross-wrap--mod-js');
         nav.classList.add('navigation--mod-js');
         navList.classList.add('navigation__list--mod-js');
-    })
+    });
     
-    crossMenu.addEventListener('touchstart', function(e){
+    crossMenu.addEventListener('touchstart', function(e) {
         burgerMenu.classList.remove('menu-burger-wrap--mod-js');
-        crossMenu.classList.remove('menu-cross-wrap--mod-js')
+        crossMenu.classList.remove('menu-cross-wrap--mod-js');
         nav.classList.remove('navigation--mod-js');
         navList.classList.remove('navigation__list--mod-js');
-    })
+    });
 })();
